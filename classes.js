fabcde300811cfce9f4035a5c8ca9a59fbb9226b
Adrian Juszczak <adrian@juszczak.io>
2022-02-13 09:20:48 +0100

    Classes pt. 2
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- classes.js
+++ classes.js
@@ -1,38 +1,55 @@
-function PersonFn(name) {
-  this.name = name;
-}
-
-const p1 = new PersonFn('Adrian1');
-
 class Person {
+  name;
+  lastname;
+
   constructor(name, lastname) {
     this.name = name;
     this.lastname = lastname;
   }
 
-  get fullName() {
+  get fullname() {
     return `${this.name} ${this.lastname}`;
   }
 
-  set fullName(value) {
-    console.log('trying to set full name', value);
+  set fullname(value) {
+    const [name, lastname] = value.split(' ');
+    this.name = name;
+    this.lastname = lastname;
   }
 
-  sayHello() {
-    console.log(`Hello ${this.fullName}!`)
+  introduceSelf() {
+    console.log(`Hi! I'm ${this.name}.`);
   }
-}
 
-const p2 = new Person('Adrian', 'Juszczak');
-
-class Counter {
-  static num = 1;
-
-  constructor() {
-    Counter.num++;
+  sayHelloTo(person) {
+    console.log(`${this.name} says "Hello!" to ${person.name}`);
   }
 }
 
-// const c1 = new Counter();
-// const c2 = new Counter();
+const o1 = {};
 
+const p1 = new Person('Adrian', 'Juszczak');
+const p2 = new Person('Ala', 'Makota');
+
+class Student extends Person {
+  static indexCounter = 1;
+
+  #index;
+
+  constructor(name, lastname) {
+    super(name, lastname);
+    this.#index = `PO00${Student.indexCounter++}`;
+  }
+
+  getStudentIndexNumber() {
+    return this.#index;
+  }
+
+  static getNextIndexNumber() {
+    return Student.indexCounter;
+  }
+}
+
+const s1 = new Student('A', 'B');
+const s2 = new Student('C', 'D');
+const s3 = new Student('E', 'F');
